769582b7751413a9a5127171575e3fc321ea22d8
iostaleksey <iost.a@avtocod.ru>
2022-04-03 17:02:37 +0200

    Device implementation

--- signing-service-challenge-go/api/device.go
+++ signing-service-challenge-go/api/device.go
@@ -1,3 +1,66 @@
 package api
 
-// TODO: REST endpoints ...
+import (
+	"encoding/json"
+	"github.com/DrMonez/coding-challenges/signing-service-challenge/domain"
+	"net/http"
+)
+
+type CreateSignatureDeviceResponse struct {
+	DeviceId string `json:"device_id"`
+	Label    string `json:"label"`
+}
+
+type CreateSignatureDeviceRequest struct {
+	Id        string                     `json:"id"`
+	Algorithm domain.CryptoAlgorithmType `json:"algorithm"`
+	Label     string                     `json:"label"`
+}
+
+type SignTransactionResponse struct {
+	Signature  string `json:"signature"`
+	SignedData string `json:"signed_data"`
+}
+
+type SignTransactionRequest struct {
+	DeviceId string `json:"device_id"`
+	Data     string `json:"data"`
+}
+
+func PostMethodTemplate[T any](request *http.Request, body *T) (isValid bool, errors *[]string) {
+	if request.Method != http.MethodPost {
+		return false, &[]string{http.StatusText(http.StatusMethodNotAllowed)}
+	}
+	decoder := json.NewDecoder(request.Body)
+	err := decoder.Decode(&body)
+	if err != nil {
+		return false, &[]string{http.StatusText(http.StatusBadRequest)}
+	}
+	return true, nil
+}
+
+func (s *Server) CreateSignatureDevice(response http.ResponseWriter, request *http.Request) {
+	var body CreateSignatureDeviceRequest
+	isValidRequest, errors := PostMethodTemplate(request, &body)
+	if isValidRequest {
+		WriteErrorResponse(response, http.StatusBadRequest, *errors)
+		return
+	}
+
+	createSignatureDeviceResponse := CreateSignatureDeviceResponse{}
+
+	WriteAPIResponse(response, http.StatusOK, createSignatureDeviceResponse)
+}
+
+func (s *Server) SignTransaction(response http.ResponseWriter, request *http.Request) {
+	var body SignTransactionRequest
+	isValidRequest, errors := PostMethodTemplate(request, &body)
+	if isValidRequest {
+		WriteErrorResponse(response, http.StatusBadRequest, *errors)
+		return
+	}
+
+	signTransactionResponse := SignTransactionResponse{}
+
+	WriteAPIResponse(response, http.StatusOK, signTransactionResponse)
+}
--- signing-service-challenge-go/api/device_test.go
+++ signing-service-challenge-go/api/device_test.go
@@ -1,1 +1,37 @@
 package api
+
+import (
+	"github.com/DrMonez/coding-challenges/signing-service-challenge/helpers"
+	"io"
+	"net/http"
+	"strings"
+	"testing"
+)
+
+func TestPostMethodTemplateWithOptionalParameter(t *testing.T) {
+	var body CreateSignatureDeviceRequest
+	json := `{ "id": "123456", "algorithm":"RSA", "label":"some label" }`
+	request := http.Request{
+		Method: http.MethodPost,
+		Body:   io.NopCloser(strings.NewReader(json)),
+	}
+	isValid, errors := PostMethodTemplate(&request, &body)
+	var expectedErrors *[]string = nil
+	helpers.ShouldBe(t, isValid, true)
+	helpers.ShouldBe(t, errors, expectedErrors)
+	helpers.ShouldBe(t, body, CreateSignatureDeviceRequest{Id: "123456", Algorithm: "RSA", Label: "some label"})
+}
+
+func TestPostMethodTemplateWithoutOptionalParameter(t *testing.T) {
+	var body CreateSignatureDeviceRequest
+	json := `{ "id": "123456", "algorithm":"RSA" }`
+	request := http.Request{
+		Method: http.MethodPost,
+		Body:   io.NopCloser(strings.NewReader(json)),
+	}
+	isValid, errors := PostMethodTemplate(&request, &body)
+	var expectedErrors *[]string = nil
+	helpers.ShouldBe(t, isValid, true)
+	helpers.ShouldBe(t, errors, expectedErrors)
+	helpers.ShouldBe(t, body, CreateSignatureDeviceRequest{Id: "123456", Algorithm: "RSA", Label: ""})
+}
